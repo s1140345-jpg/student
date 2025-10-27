bf5b2bfc0a4bc4fb19ba30518d9f264bbf6ffa66
s1140345-jpg <s1140345@dmhs.kh.edu.tw>
2025-10-27 16:32:28 +0800

    hw2

---
+++ hw2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+    int age = 16;
+    double heeight = 171;
+    std::string name = "呂博名";
+    bool is_student = false;
+    std::cout << "姓名: " << name << std::endl;
+    std::cout << "身高: " << heeight << std::endl;
+    std::cout << "年齡: " << age << std::endl;
+    std::cout << "是否為學生;" << is_student << std::endl;
+    
+    
+    return 0;
+}
