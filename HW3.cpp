2b253983399a85b656214b89874d93cb4ab57857
s1140345-jpg <s1140345@dmhs.kh.edu.tw>
2025-11-07 11:52:36 +0800

    Add files via upload

---
+++ HW3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Iamgod台股大跌" << std::endl;
+    std::cout << "I am god 台股大跌" << std::endl;
+    std::cout << "I\ttam\tgod 台股\t大跌" << std::endl;
+    std::cout << "I\namgod 台股\n大跌" << std::endl;
+    std::cout << "\"I am god 台股大跌\"" << std::endl;
+    std::cout << "\\I am god 台股大跌\\\\" << std::endl;
+
+    return 0;
+}
