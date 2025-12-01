05bc7678eae0eda109470ddae289c8ed5c0b9968
s1140345-jpg <s1140345@dmhs.kh.edu.tw>
2025-12-01 16:20:36 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+using namespace std;
 
-int main(){
-    std::cout <<  "Hello World"<< std::endl;
+int main() {
+    int n;
+    cout << "輸入 n: ";
+    cin >> n;
+
+    int sum = 0;
+
+    for (int i = 1; i <= n; i++) {
+        sum += i;
+    }
+
+    cout << "1 到 " << n << " 的加總 = " << sum << endl;
+
     return 0;
 }
